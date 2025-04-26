a1a6ac7128cf97776fc4be1575fbca99752bd98a
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-04-26 11:12:41 +0800

    fixed issue

--- src/app/head/(routes)/purchase-request/page.tsx
+++ src/app/head/(routes)/purchase-request/page.tsx
@@ -17,15 +17,11 @@ const Page = async () => {
       createdAt: "desc",
     },
     include: {
-      Employee: true,
+      receivedEmployee: true,
+      requestedBy: true,
       PurchaseRequestItem: {
         include: {
           Item: true,
-        },
-      },
-      ReceivedBy: {
-        include: {
-          Employee: true,
         },
       },
     },
@@ -45,8 +41,8 @@ const Page = async () => {
 
       return {
         id: request.id,
-        licenseNo: request.Employee.licenseNo,
-        name: `${request.Employee.firstName} ${request.Employee.middleName || ""} ${request.Employee.lastName}`.trim(),
+        licenseNo: request.requestedBy.licenseNo,
+        name: `${request.requestedBy.firstName} ${request.requestedBy.middleName || ""} ${request.requestedBy.lastName}`.trim(),
         itemName: itemNames,
         quantity: request.PurchaseRequestItem.reduce(
           (acc, prItem) => acc + prItem.quantity,
