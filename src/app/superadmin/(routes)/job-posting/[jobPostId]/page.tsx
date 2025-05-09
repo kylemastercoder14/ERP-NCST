c62869c8a9b1e0fed86e2c55a2afbf9fe7e6678d
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-05-09 14:29:00 +0800

    fixed eslint issue

--- src/app/superadmin/(routes)/job-posting/[jobPostId]/page.tsx
+++ src/app/superadmin/(routes)/job-posting/[jobPostId]/page.tsx
@@ -1,28 +1,45 @@
 import React from "react";
 import db from "@/lib/db";
 import JobPostingForm from "@/components/forms/job-posting-form";
-import { useUser } from '@/hooks/use-user';
+import { useUser } from "@/hooks/use-user";
 
 const Page = async (props: {
   params: Promise<{
-	jobPostId: string;
+    jobPostId: string;
   }>;
 }) => {
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	  const { user } = await useUser();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { user } = await useUser();
   const params = await props.params;
   const jobPosting = await db.jobPosting.findUnique({
-	where: {
-	  id: params.jobPostId,
-	},
+    where: {
+      id: params.jobPostId,
+    },
   });
 
   const department = user?.Employee?.Department.name;
 
+  const departments = await db.department.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  const jobPositions = await db.jobTitle.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
   return (
-	<div>
-	  <JobPostingForm initialData={jobPosting} department={department as string} />
-	</div>
+    <div>
+      <JobPostingForm
+        initialData={jobPosting}
+        department={department as string}
+        departments={departments}
+        jobPositions={jobPositions}
+      />
+    </div>
   );
 };
 
